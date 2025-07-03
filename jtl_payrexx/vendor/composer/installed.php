dd9104beb06fbefaf067f45ebd1159c9a4d8637e
vinoth <vinoth@payrexx.com>
2025-07-03 19:12:08 +0530

    task/PP-15938: updated php sdk 2.0

--- jtl_payrexx/vendor/composer/installed.php
+++ jtl_payrexx/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'payrexx_gateway/jtl_payrexx',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '069ba6e11a439d36182132e4e67e1fef1562c610',
+        'reference' => 'c110b963ff0462e0f3ff3ac006ffe4a660c2d24b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'payrexx/payrexx' => array(
-            'pretty_version' => 'v1.8.10',
-            'version' => '1.8.10.0',
-            'reference' => 'ab932dca32c607cbaa6f8da9b9e1de2efe28021f',
+            'pretty_version' => 'v2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => 'b176b906b99b4be1b8059a2a6ec96f40ecdda7f6',
             'type' => 'library',
             'install_path' => __DIR__ . '/../payrexx/payrexx',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'payrexx_gateway/jtl_payrexx' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '069ba6e11a439d36182132e4e67e1fef1562c610',
+            'reference' => 'c110b963ff0462e0f3ff3ac006ffe4a660c2d24b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
